63077f33ba7d0a2fa4d4865c00ed30f8488d636c
Andrey Likhomsn <andrey.likhoman@gmail.com>
2015-09-30 12:50:49 +0300

    chenage setting

--- grapeSite/settings.py
+++ grapeSite/settings.py
@@ -109,7 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 
 MEDIA_URL = '/media/'
 
