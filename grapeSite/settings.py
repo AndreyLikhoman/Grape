a34220117b5947d5012a4a007ff9c109b57a743f
Andrey Lykhoman <andrey.likhoman@gmail.com>
2015-10-02 14:24:38 +0300

    change seting

--- grapeSite/settings.py
+++ grapeSite/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = '/media/'
