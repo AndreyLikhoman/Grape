856677c93d65fe4cfbfaf553fbb4d6c932ab3850
Andrey Lykhoman <andrey.likhoman@gmail.com>
2015-10-08 16:41:38 +0300

    add registr

--- blog/registration.py
+++ blog/registration.py
@@ -1,21 +1,25 @@
 from django.contrib import auth
 from django.http import *
+
 def login(request):
-    email = request.POST['email']
-    password = request.POST['password']
-    user = auth.authenticate(email=email, password=password)
-    if user is not None and user.is_active:
-        # Правильный пароль и пользователь "активен"
-        auth.login(request, user)
-        # Перенаправление на "правильную" страницу
-        return HttpResponseRedirect("/account/loggedin/")
-    else:
-        # Отображение страницы с ошибкой
-        return HttpResponseRedirect("/account/invalid/")
+	username = request.POST['username']
+	password = request.POST['password']
+	user = auth.authenticate(username=username, password=password)
+	if user is not None and user.is_active:
+		auth.login(request, user)
+		return HttpResponsePermanentRedirect('')
+	else:
+		return HttpResponsePermanentRedirect('')
 
 
 
 def logout(request):
     auth.logout(request)
     # Перенаправление на страницу.
-    return HttpResponseRedirect("/account/loggedout/", template_name ='blog/account/registration.html')
+    return HttpResponseRedirect("/account/loggedout/")
+
+def is_true(request, username):
+	return render(request, 'blog/account/registration.html')
+
+def is_false(request):
+	return render(request, 'blog/account/registration.html')
--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,13 @@ from . import views, registration
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
+    url(r'^personal/$',  registration.login , name="login"),
+    url(r'^personal/(?P<id>[\w -]+)^$', registration.is_true, name='is_true'),
+    url(r'/fff/^$', registration.is_false, name='is_false'),
     url(r'^galereya/$', views.return_galeries, name='return_galeries'),
     url(r'^galereya/(?P<url>[\w -]+)/$', views.galery_detail, name='galery_detail'),
     url(r'^media/$', views.return_medias, name='return_medias'),
     url(r'^media/(?P<url>[\w -]+)/$', views.return_videos, name='return_videos'),
-    url(r'^accounts/login/$',  registration.login),
     url(r'^accounts/logout/$', registration.logout),
     url(r'^(?P<category>[\w -]+)/(?P<url>[\w -]+)/$', views.post_detail, name='post_detail'),
     url(r'^(?P<url>[\w -]+)/$', views.category_detail, name='category_detail'),
